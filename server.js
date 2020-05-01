88aef0b23e9ff1f200609b607dcd73da34b9231e
Tommy Li <tommyli3318@gmail.com>
2020-05-01 15:28:46 -0700

    more functionality and improvements

--- server.js
+++ server.js
@@ -18,15 +18,11 @@ app.get('/', (req, res) => {
 // }
 // returns a number indicating distance in miles between the two locations
 app.post('/', (req, res) => {
-    axios.post('http://www.mapquestapi.com/directions/v2/route?key=PnhXYN4xTR8F3bexWpuVOi4olDU5kIla', {
-        locations: [req.body.origin, req.body.destination]
-    })
-    .then(response => {
-        res.send(response.data.route.distance.toString());
-    })
-    .catch(err => {
-        console.error(err);
-    })
+    // Async scope to use await
+    (async () => {
+        distance = await getDistanceBetween(req.body.origin, req.body.destination);
+        res.send(distance.toString());
+    })();
 })
 
 
@@ -42,17 +38,17 @@ app.post('/', (req, res) => {
 // WHERE trip_expense_total > 25000
 
 app.get('/expensiveTrips', (req, res) => {
-    // Async scope so await can be used
+    // Async scope to use await
     (async () => {
         let expensiveContactIds = new Set(); // Stores Contact Ids of customers with trip expense > 25000
-        for (var trip of await csvAsJson('./fake_finity_data/trip_data.csv')) {
+        for (let trip of await csvAsJson('./fake_finity_data/trip_data.csv')) {
             if (parseInt(trip['trip_expense_total'].substring(1)) > 25000) {
                 expensiveContactIds.add(trip['contact_id']);
             }
         }
 
-        result = []; // Stores firstname,lastname,email of customers with trip expense > 25000
-        for (var contact of await csvAsJson('./fake_finity_data/contacts.csv')) {
+        let result = []; // Stores firstname,lastname,email of customers with trip expense > 25000
+        for (let contact of await csvAsJson('./fake_finity_data/contacts.csv')) {
             if (expensiveContactIds.has(contact['id'])) {
                 // this customer had an expensive trip
                 result.push({
@@ -67,15 +63,48 @@ app.get('/expensiveTrips', (req, res) => {
     })();
 })
 
+
+// GET to http://localhost:8080/tripDistances returns
+// list of distances between the origin and destinations within the trip_data using MapQuest API
+// Currently times out due to MapQuest API being too slow
+app.get('/tripDistances', (req, res) => {
+    // MapQuest docs for POST with latLng
+    // https://developer.mapquest.com/documentation/common/forming-locations/
+    (async () => {
+        let distances = [];
+        for (let trip of await csvAsJson('./fake_finity_data/trip_data.csv')) {
+            let origin = {
+                "latLng": {
+                  "lat": parseFloat(trip['trip_origin_latitude']),
+                  "lng": parseFloat(trip['trip_origin_longitude'])
+                }
+            }
+
+            let destination  = {
+                "latLng": {
+                  "lat": parseFloat(trip['trip_dest_latitude']),
+                  "lng": parseFloat(trip['trip_dest_longitude'])
+                }
+            }
+
+            distances.push(await getDistanceBetween(origin, destination));
+        }
+
+        res.send(distances);
+    })();
+})
+
+
 async function csvAsJson(path) {
     return csvtojson().fromFile(path);
 }
 
-
-app.get('/tripDistances', (req, res) => {
-    // TODO: return a list of distances between the origin and destinations within the trip_data using MapQuest API
-    // Maybe make a new function(origin, destination) to calculate distance using Mapquest?
-})
+async function getDistanceBetween(origin, destination) {
+    let response = await axios.post('http://www.mapquestapi.com/directions/v2/route?key=PnhXYN4xTR8F3bexWpuVOi4olDU5kIla', {
+        locations: [origin, destination]
+    })
+    return response.data.route.distance;
+}
 
 
 const port = process.env.PORT || 8080;
